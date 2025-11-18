8d2d34f9118d80f4e350f2d141b8ab104fb4abc7
Josué Weschenfelder <josuweschen@gmail.com>
2025-11-17 21:45:44 -0300

    Ajustado o evento-service
    Criado os getters e setters abandonando o lombok para corrigir de forma rápida

--- JAVA/eventos-service/src/main/java/com/eventhub/eventos_service/model/Evento.java
+++ JAVA/eventos-service/src/main/java/com/eventhub/eventos_service/model/Evento.java
@@ -1,22 +1,50 @@
 package com.eventhub.eventos_service.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
 @Table(name = "eventos")
-@Data
 public class Evento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //private String data;
-    
     private String descricao;
-    
     private String local;
-    
     private String nome;
+
+    // GETTERS E SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
